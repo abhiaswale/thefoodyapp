fe157724ff057553587f209870ae3f9d0e4e1b98
abhiaswale <aswaleabhishek8@gmail.com>
2021-10-13 16:18:31 +0530

    modal & backdrop added

---
+++ src/Components/Ui/Modal.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./Mofal.module.css";
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+const portalElement = document.getElementById("overlay");
+function Modal() {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+}
+
+export default Modal;
