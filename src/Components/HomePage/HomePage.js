1acab087ed83997e94f1b03efbdba35d358a4172
abhiaswale <aswaleabhishek8@gmail.com>
2021-10-27 11:36:17 +0530

    Added map

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -126,6 +126,7 @@ const HomePage = () => {
       </section>
       <div className={classes.map}>
         <iframe
+          //
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5633.503182277999!2d73.80081686567132!3d19.991665527522077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddeae340b32e15%3A0xcf37e8d2529426e4!2sDomino&#39;s%20Pizza!5e0!3m2!1sen!2sin!4v1633413780686!5m2!1sen!2sin"
           style={{
             width: "99.6%",
